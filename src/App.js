60a5a9c9bbad7b643ec6748fba703fa045a779d1
KimJuhyoung22 <juhyoung22@handong.ac.kr>
2025-11-15 22:51:09 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>OSS-assignment#4-2</h1>
+
+        <p>22200177 김주형입니다.</p>
+
       </header>
     </div>
   );
